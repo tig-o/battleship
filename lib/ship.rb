d38a79fa0c7a4e002aafffa814af30771c7cc936
Thiago Silveira <thiago1992@yahoo.com>
2022-05-17 22:10:17 -0600

    Feat: Pass test ship has health

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,7 +1,8 @@
 class Ship
-  attr_reader :name, :length
+  attr_reader :name, :length, :health
   def initialize(ship_name, ship_length)
     @name = ship_name
     @length = ship_length
+    @health = @length
   end
 end
