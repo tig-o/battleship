3457a56c571317fd31cf68febe80f0fcd16374ac
Thiago Silveira <thiago1992@yahoo.com>
2022-05-17 22:08:01 -0600

    Test: Ship has a length

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -5,9 +5,15 @@ RSpec.describe Ship do
     cruiser = Ship.new("Cruiser", 3)
     expect(cruiser).to be_a(Ship)
   end
-  
+
   it 'has a name' do
     cruiser = Ship.new("Cruiser", 3)
     expect(cruiser.name).to eq("Cruiser")
   end
+
+  it 'has a length' do
+    cruiser = Ship.new("Cruiser", 3)
+    expect(cruiser.length).to eq(3)
+  end
+  
 end
