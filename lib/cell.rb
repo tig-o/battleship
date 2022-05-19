dfca9ad9bd91c31fcd1623d80c1d6e676e36d278
Riley McCullough <rileybmccullough@gmail.com>
2022-05-19 16:36:07 -0600

    Feat: cell can be fired upon

--- lib/cell.rb
+++ lib/cell.rb
@@ -3,6 +3,8 @@ class Cell
   def initialize(grid_coordinate)
     @coordinate = grid_coordinate
     @ship = nil # created a path and forcing it to be nil by default
+    @fired_upon = false
+
   end
 
   def empty?
@@ -15,6 +17,15 @@ class Cell
 
   def place_ship(instance_of_ship)
     @ship = instance_of_ship # storing object in ship variable
+  end
+
+  def fired_upon?
+    if @fired_upon == false
+      false
+    elsif @fired_upon != false
+      true
+    end
+
   end
 
 end
