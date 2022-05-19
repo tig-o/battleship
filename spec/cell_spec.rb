6a3d2378b4135b6713df2dbab05aef96fd2ce34b
Thiago Silveira <thiago1992@yahoo.com>
2022-05-19 11:20:54 -0600

    Test: Cell can check if ship is on cell and returns empty status

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -13,23 +13,10 @@ RSpec.describe Cell do
     expect(cell.coordinate).to eq("B4")
   end
 
-  it "has a ship" do
+  it "can check if ship is on cell and returns if cell is empty" do
     cell = Cell.new("B4")
     expect(cell.ship).to eq(nil)
-  end
-
-  it "can be empty" do
-    cell = Cell.new("B4")
     expect(cell.empty?).to eq(true)
-  end
-
-  it "can occupy cell with a ship" do
-    cell = Cell.new("B4")
-    cruiser = Ship.new("Cruiser", 3)
-
-    
-    expect(cell.ship).to eq(cruiser)
-
   end
 
 end
