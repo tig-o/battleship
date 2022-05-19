751dca56a67cd38923506c8fc3e95d6c57059978
Riley McCullough <rileybmccullough@gmail.com>
2022-05-18 18:47:49 -0600

    Test: cell has a coordinate

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -5,8 +5,12 @@ RSpec.describe Cell do
 
   it "exists" do
     cell = Cell.new("B4")
-    expect (cell).to be_a(Cell)
+    expect(cell).to be_a(Cell)
   end
 
+  it "has a coordinate" do
+    cell = Cell.new("B4")
+    expect(cell.coordinate).to eq("B4")
+  end
 
 end
