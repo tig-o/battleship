ed7e71580decea6ca44791a0f8dc6c4b9188a68d
Thiago Silveira <thiago1992@yahoo.com>
2022-05-19 14:03:45 -0600

    Test: Edge case cell can place instance of a ship

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -27,4 +27,26 @@ RSpec.describe Cell do
     expect(cell.empty?).to eq(false)
   end
 
+  it 'edge case, can place instance of a ship' do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    sub = Ship.new("Red October", 2)
+    pt_boat = Ship.new("Zeus", 1)
+    cell.place_ship(cruiser)
+    expect(cell.ship).to eq(cruiser)
+    expect(cell.empty?).to eq(false)
+
+    cell.place_ship(nil)
+    expect(cell.empty?).to eq(true)
+    cell.place_ship(sub)
+    expect(cell.ship).to eq(sub)
+    expect(cell.empty?).to eq(false)
+
+    cell.place_ship(nil)
+    expect(cell.empty?).to eq(true)
+    cell.place_ship(pt_boat)
+    expect(cell.ship).to eq(pt_boat)
+    expect(cell.empty?).to eq(false)
+  end
+
 end
