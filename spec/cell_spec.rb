e86e340bf4f506f043ddbaf430c6abe9bb623fa3
Riley McCullough <rileybmccullough@gmail.com>
2022-05-19 16:35:24 -0600

    Test: cell can be fired upon

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -49,4 +49,10 @@ RSpec.describe Cell do
     expect(cell.empty?).to eq(false)
   end
 
+  it "can be been fired upon" do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    expect(cell.fired_upon?).to eq(false)
+  end
 end
