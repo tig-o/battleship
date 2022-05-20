3cb933f680d26baf423460c8fe58c8d2a90b9043
Thiago Silveira <thiago1992@yahoo.com>
2022-05-20 16:56:10 -0600

    Test: Cell can render and mark a miss

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -65,4 +65,11 @@ RSpec.describe Cell do
     expect(cell.fired_upon?).to eq(true)
   end
 
+  it 'can render a cell and return a miss' do
+    cell_1 = Cell.new("B4")
+    expect(cell_1.render).to eq(".")
+    cell_1.fire_upon
+    expect(cell_1.render).to eq("M")
+  end
+
 end
