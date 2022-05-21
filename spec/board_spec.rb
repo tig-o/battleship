bd61d1dea1497bf2634b1e122b21c3d65dbcbaaf
Riley McCullough <rileybmccullough@gmail.com>
2022-05-21 15:43:31 -0600

    Test: can have board hash holding cells

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,11 +1,18 @@
 require './lib/board'
 require 'pry'
+require './lib/cell'
+require './lib/ship'
 
 RSpec.describe Board do
 
   it 'exists' do
-    bored = Board.new
-    expect(bored).to be_a(Board)
+    board = Board.new
+    expect(board).to be_a(Board)
   end
 
+  it 'can have a board hash where cells exist' do
+    board = Board.new
+    expect(board.board_hash["C4"]).to be_a(Cell)
+
+  end
 end
