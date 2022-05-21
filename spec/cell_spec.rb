7ac30cb413ba3d77a04e84abd3ca67da7432dc24
Thiago Silveira <thiago1992@yahoo.com>
2022-05-20 18:20:04 -0600

    Test: Cell can render and mark a hit

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -72,4 +72,15 @@ RSpec.describe Cell do
     expect(cell_1.render).to eq("M")
   end
 
+  it 'can render a cell and return a hit' do
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+    expect(cell_2.render).to eq(".")
+    # expect(cell_2.render(true)).to eq("S")
+    cell_2.fire_upon
+    expect(cell_2.render).to eq("H")
+    expect(cruiser.health).to eq(2)
+  end
+
 end
