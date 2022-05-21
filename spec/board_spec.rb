91f454d9ff75e6808a95c6e11c93f647b01479ef
Riley McCullough <rileybmccullough@gmail.com>
2022-05-21 17:52:48 -0600

    Test: validate array coordinates against ship length

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -34,4 +34,14 @@ RSpec.describe Board do
    expect(board.valid_coordinate?("A22")).to eq(false)
   end
 
+  it 'can validate array coordinates against ship length' do
+   board = Board.new
+   cruiser = Ship.new("Cruiser", 3)
+   submarine = Ship.new("Submarine", 2)
+
+   expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
+
+   expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+ end
+
 end
