920924d544ac1ea0d9a20ae909e30c577dd1cabc
Thiago Silveira <thiago1992@yahoo.com>
2022-05-23 20:41:47 -0600

    Test: Board can check that coordinates are diagonal and return false

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -33,7 +33,7 @@ RSpec.describe Board do
     expect(board.valid_coordinate?("A22")).to eq(false)
   end
 
-  xit 'can validate array coordinates against ship length' do
+  it 'can validate array coordinates against ship length' do
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
@@ -48,9 +48,19 @@ RSpec.describe Board do
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
     expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
-    # expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
     expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
-    # expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
- end
+    expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
+  end
+
+  it 'can check that coordinates are diagonal and return false' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
+  end
+
+
 
 end
