1ec562928aeef8e292f1c5bbee51f463f2c96091
Thiago Silveira <thiago1992@yahoo.com>
2022-05-25 12:28:19 -0600

    Test: Player can use a board and ships

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe Player do
     player1 = Player.new
     expect(player1).to be_a(Player)
   end
+
+  it 'can use a board and ships' do
+    player_board = Board.new
+    expect(player_board).to be_a(Board)
+
+    player_sub = Ship.new("Submarine", 2)
+    expect(player_sub).to be_a(Ship)
+
+    player_cruiser = Ship.new("Cruiser", 3)
+    expect(player_cruiser).to be_a(Ship)
+  end
 end
