354c3eb9399d5ec027f2c22d79c5cca2a0c8e077
Thiago Silveira <thiago1992@yahoo.com>
2022-05-25 12:04:35 -0600

    Refactor: Feature pass complete test where computer places cruiser type ship on board and validates placement

--- lib/computer.rb
+++ lib/computer.rb
@@ -9,27 +9,20 @@ class Computer
   def coordinate_generator
     random_num = rand(1..4).to_s
     random_letter = ["A", "B", "C", "D"].shuffle.first
-    random_element = random_letter + random_num
+    random_letter + random_num
   end
 
-  def place_ship(ship)
-    if ship.length == 2
-      two_coordinates = []
-        until @cpu_board.valid_placement?(ship, two_coordinates)
-          2.times do
-            two_coordinates << coordinate_generator
-          end
+  def place_cruiser(cruiser)
+    three_coordinates = []
+      until @cpu_board.valid_placement?(cruiser, three_coordinates)
+        1.times do
+          three_coordinates = []
+          three_coordinates << coordinate_generator
+          three_coordinates << coordinate_generator
+          three_coordinates << coordinate_generator
         end
-      @cpu_board.place(ship, two_coordinates)
-    elsif ship.length == 3
-      three_coordinates = []
-        until @cpu_board.valid_placement?(ship, three_coordinates)
-          3.times do
-            three_coordinates << coordinate_generator
-          end
-        end
-      @cpu_board.place(ship, three_coordinates)
-    end
+      end
+    @cpu_board.place(cruiser, three_coordinates)
   end
 
 end
