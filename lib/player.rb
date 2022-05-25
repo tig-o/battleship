b44e29a00de63b63cf3f836d1e72835c73db4aaf
Thiago Silveira <thiago1992@yahoo.com>
2022-05-25 12:47:32 -0600

    Feat: Player inputs their coordinates

--- lib/player.rb
+++ lib/player.rb
@@ -4,4 +4,11 @@ class Player
     @player_sub = Ship.new("Submarine", 2)
     @player_cruiser = Ship.new("Cruiser", 3)
   end
+
+  def coordinate_input
+    player_coordinates = []
+    user_input = gets.chomp
+    player_coordinates << user_input
+  end
+
 end
