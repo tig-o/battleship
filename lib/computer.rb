071d83618e6d5e537cc6b2115528d2f8da7e281d
Thiago Silveira <thiago1992@yahoo.com>
2022-05-24 22:03:54 -0600

    Feat: Computer generates random coordinates

--- lib/computer.rb
+++ lib/computer.rb
@@ -6,5 +6,10 @@ class Computer
     @cpu_cruiser = Ship.new("Cruiser", 3)
   end
 
+  def coordinate_generator
+    random_num = rand(1..4).to_s
+    random_letter = ["A", "B", "C", "D"].shuffle.first
+    random_element = random_letter + random_num
+  end
 
 end
