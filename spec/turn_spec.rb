8af22c2295718a29b91c399f7769b7d56940c612
Riley McCullough <rileybmccullough@gmail.com>
2022-05-25 16:05:08 -0600

    Fix the runner, not funtional yet

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,13 +1,10 @@
-require './lib/ship'
-require './lib/cell'
-require './lib/board'
-require './lib/computer'
-require './lib/player'
-require './lib/turn'
 
 RSpec.describe Turn do
   it 'exists' do
     turn = Turn.new
     expect(turn).to be_a(Turn)
   end
+
+
+
 end
